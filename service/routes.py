c1ff2cb88060fbcafb9c66574e791f69a9d73228
ElioZhou <yzzhou1999@gmail.com>
2022-10-18 18:59:20 +0000

    modified

--- service/routes.py
+++ service/routes.py
@@ -41,26 +41,7 @@ def index():
         status.HTTP_200_OK,
     )
 
-
 ######################################################################
-# RETRIEVE A PPRODUCT
-######################################################################
-@app.route("/products/<int:product_id>", methods=["GET"])
-def get_products(product_id):
-    """
-    Retrieve a single Product
-
-    This endpoint will return a Product based on it's id
-    """
-    app.logger.info("Request for product with id: %s", product_id)
-    product = Product.find(product_id)
-    if not product:
-        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
-
-    app.logger.info("Returning product: %s", product.name)
-    return jsonify(product.serialize()), status.HTTP_200_OK
-
- ######################################################################
 # ADD A NEW PRODUCT
 ######################################################################
 @app.route("/products", methods=["POST"])
@@ -82,32 +63,6 @@ def create_products():
 
 
 ######################################################################
-# UPDATE AN EXISTING PRODUCT
-######################################################################
-@app.route("/products/<int:product_id>", methods=["PUT"])
-def update_products(product_id):
-    """
-    Update a Product
-
-    This endpoint will update a Product based the body that is posted
-    """
-    app.logger.info("Request to update product with id: %s", product_id)
-    check_content_type("application/json")
-
-    product = Product.find(product_id)
-    if not product:
-        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
-
-    product.deserialize(request.get_json())
-    product.id = product_id
-    product.update()
-
-    app.logger.info("Product with ID [%s] updated.", product.id)
-    return jsonify(product.serialize()), status.HTTP_200_OK
-
-
-
-######################################################################
 # RETRIEVE A PPRODUCT
 ######################################################################
 @app.route("/products/<int:product_id>", methods=["GET"])
@@ -125,25 +80,6 @@ def get_products(product_id):
     app.logger.info("Returning product: %s", product.name)
     return jsonify(product.serialize()), status.HTTP_200_OK
 
-######################################################################
-# ADD A NEW PRODUCT
-######################################################################
-
-@app.route("/products", methods=["POST"])
-def create_products():
-    """
-    Creates a Product
-    This endpoint will create a Product based the data in the body that is posted
-    """
-    app.logger.info("Request to create a product")
-    check_content_type("application/json")
-    product = Product()
-    product.deserialize(request.get_json())
-    product.create()
-    message = product.serialize()
-    location_url = url_for("get_products", product_id=product.id, _external=True)
-    app.logger.info("Product with ID [%s] created.", product.id)
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
 
 ######################################################################
