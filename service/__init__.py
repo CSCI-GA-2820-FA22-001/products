6512cf16d68d3230d921f26c906d865a03d589d4
marfa0414 <1021785867@qq.com>
2022-10-22 02:42:35 +0000

    change error handler

--- service/__init__.py
+++ service/__init__.py
@@ -15,8 +15,8 @@ app.config.from_object(config)
 
 # Dependencies require we import the routes AFTER the Flask app is created
 # pylint: disable=wrong-import-position, wrong-import-order
-from service import routes         # noqa: E402, E261
-# pylint: disable=wrong-import-position
+from service import routes,models         # noqa: E402, E261
+# pylint: disable=wrong-import-positions
 from .common import error_handlers  # noqa: F401 E402
 
 # Set up logging for production
@@ -27,7 +27,7 @@ app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
 
 try:
-    routes.init_db()  # make our SQLAlchemy tables
+    models.init_db(app)  # make our SQLAlchemy tables
 except Exception as error:
     app.logger.critical("%s: Cannot continue", error)
     # gunicorn requires exit code 4 to stop spawning workers when they die
