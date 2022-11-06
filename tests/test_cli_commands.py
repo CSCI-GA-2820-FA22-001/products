e90078ee972ee600f8d4f0b3b3fe9caf66f4ed10
Haohui Shen <38171484+marfa0414@users.noreply.github.com>
2022-11-05 21:51:17 -0400

    Update test_cli_commands.py
    add import os

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 from unittest.mock import patch, MagicMock
 from click.testing import CliRunner
 from service.common.cli_commands import create_db
-
+import os
 
 class TestFlaskCLI(TestCase):
     """Test Flask CLI Commands"""
