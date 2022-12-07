d3fb82d6b2b8e78f4b8dd9871c49a5358a034d98
Liu Yunrui <roy.liu@126.com>
2022-12-06 20:49:42 -0500

    Update rest_api.js

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -62,36 +62,8 @@ $(function () {
         $("#flash_message").append(message);
     }
 
-    // ****************************************
-    // Create a Product
-    // ****************************************
 
-    $("#list all-btn").click(function () {
-
-        let product_id = $("#product_id").val();
-
-        $("#flash_message").empty();
-
-        let ajax = $.ajax({
-            type: "GET",
-            url: `/products/${product_id}`,
-            contentType: "application/json",
-            data: ''
-        })
-
-        ajax.done(function(res){
-            //alert(res.toSource())
-            update_form_data(res)
-            flash_message("Success")
-        });
-
-        ajax.fail(function(res){
-            clear_form_data()
-            flash_message(res.responseJSON.message)
-        });
-
-    });
-
+    
     $("#create-btn").click(function () {
 
         let name = $("#product_name_for_create").val();
